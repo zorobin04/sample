c5ab63d8a1fb9ba5387c3d4775edd585055f2755
zorobin04 <nggopinath04@gmail.com>
2026-03-23 13:00:17 +0530

    good

---
+++ CacheDemo/Controllers/ProductController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace CacheDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IMemoryCache _cache;
+        private readonly ProductService _service;
+
+        public ProductController(IMemoryCache cache, ProductService service)
+        {
+            _cache = cache;
+            _service = service;
+        }
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            string cacheKey = "productList";
+
+            if (_cache.TryGetValue(cacheKey, out List<string> products))
+            {
+                return Ok(new
+                {
+                    source = "cache",
+                data= products});
+            }
+            //from db
+
+            products = _service.GetProducts();
+            var cacheEntryOptions = new MemoryCacheEntryOptions()
+                .SetSlidingExpiration(TimeSpan.FromSeconds(5));
+            _cache.Set(cacheKey, products, cacheEntryOptions);
+            return Ok(new
+            {
+                source = "database",
+                data = products
+            });
+
+        }
+    }
+}
---
+++ CacheDemo/ProductService.cs
@@ -0,0 +1,12 @@
+﻿namespace CacheDemo
+{
+    public class ProductService
+    {
+        public List<string> GetProducts()
+        {
+            Thread.Sleep(3000); // Simulate a delay in fetching products
+            return new List<string> { "Perfume", "Watch", "Keyboard" };
+        }
+
+    }
+}
